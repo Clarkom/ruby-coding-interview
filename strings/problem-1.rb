ef901c4f319a07e687846c52e5cd445dcba246e4
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-11-07 18:40:07 +0000

    Add First Strings Problems

---
+++ strings/problem-1.rb
@@ -0,0 +1,13 @@
+# 1 - Intialize a new string, "ABC", abd store it in a new variable called a_string
+a_string = "ABC"
+puts a_string
+
+# 2 - From the string called b_string, defined below, retriveve and print the second character
+b_string = "DEF"
+puts b_string[1]
+
+# 3 - Iterate over each character in c_string and print it using the "for i ..." syntax
+c_string = "GHI"
+for i in 0..(c_string.length)
+  puts c_string[i]
+end
