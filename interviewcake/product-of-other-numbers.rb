850fc53b7dcc8bf6249f6919726e234a6ccbd0c6
Arbaoui Mehdi <arbaoui.mehdi@gmail.com>
2017-11-14 20:00:28 +0000

    Add Product of All The Numbers

---
+++ interviewcake/product-of-other-numbers.rb
@@ -0,0 +1,7 @@
+# You have an array of integers, and for each index
+# you want to find the product of every integer
+# except the integer at the current index
+def get_the_product_of_all_ints_except_at_index(list)
+
+end
+
